a142370cf05f02861ec8d5849102b3badd9535f0
wangxinyu <xinyu.wang@centurygame.com>
2021-06-19 19:26:20 +0800

    add

--- src/config/env.js
+++ src/config/env.js
@@ -6,17 +6,17 @@
  * baseImgPath: 图片存放地址
  * 
  */
-let baseUrl = ''; 
+let baseUrl = 'http://localhost:8082'; 
 let routerMode = 'hash';
 let baseImgPath;
 
-if (process.env.NODE_ENV == 'development') {
-	baseUrl = '';
-    baseImgPath = '/img/';
-}else{
-	baseUrl = '//elm.cangdu.org';
-    baseImgPath = '//elm.cangdu.org/img/';
-}
+// if (process.env.NODE_ENV == 'development') {
+// 	baseUrl = 'http://localhost:8082';
+//     baseImgPath = '/img/';
+// }else{
+// 	baseUrl = '//elm.cangdu.org';
+//     baseImgPath = '//elm.cangdu.org/img/';
+// }
 
 export {
 	baseUrl,
